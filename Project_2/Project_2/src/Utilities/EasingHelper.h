6fdda419048475a0dc963ac15b09cd1649f0a10f
Asskicker25 <sp68860@gmail.com>
2023-12-07 15:52:48 -0500

    All easing modes

--- Project_2/Project_2/src/Utilities/EasingHelper.h
+++ Project_2/Project_2/src/Utilities/EasingHelper.h
@@ -6,13 +6,20 @@
 #define M_PI 3.14159265358979323846
 #endif
 
+static constexpr float c1 = 1.70158;
+static constexpr float c3 = c1 + 1;
 static constexpr float c4 = (2 * M_PI) / 3;
 
 static enum Ease_Mode
 {
 	Sine,
+	Quad,
 	Cubic,
+	Quart,
+	Quint,
 	Expo,
+	Circ,
+	Back,
 	Elastic,
 	Bounce
 };
@@ -44,11 +51,26 @@ static float EaseIn(Ease_Mode mode, float time)
 	case Sine:
 		return 1 - std::cos((time * M_PI) / 2);
 
+	case Quad:
+		return time * time;
+
 	case Cubic:
 		return time * time * time;
 
+	case Quart:
+		return time * time * time * time;
+
+	case Quint:
+		return time * time * time * time * time;
+
 	case Expo:
 		return time == 0 ? 0 : std::pow(2, 10 * time - 10);
+
+	case Circ:
+		return 1 - std::sqrt(1 - std::pow(time, 2));
+
+	case Back:
+		return c3 * time * time * time - c1 * time * time;
 
 	case Elastic:
 
@@ -74,12 +96,27 @@ static float EaseOut(Ease_Mode mode, float time)
 	{
 	case Sine:
 		return std::sin((time * M_PI) / 2);
+
+	case Quad:
+		return 1 - (1 - time) * (1 - time);
 	
 	case Cubic:
 		return 1 - std::pow(1 - time, 3);
 
+	case Quart:
+		return 1 - std::pow(1 - time, 4);
+
+	case Quint:
+		return 1 - std::pow(1 - time, 5);
+
 	case Expo:
 		return time == 1 ? 1 : 1 - std::pow(2, -10 * time);
+
+	case Circ:
+		return std::sqrt(1 - std::pow(time - 1, 2));
+
+	case Back:
+		return 1 + c3 * std::pow(time - 1, 3) + c1 * std::pow(time - 1, 2);
 
 	case Elastic:
 		return time == 0
@@ -104,13 +141,33 @@ static Ease_Mode GetMode(const std::string& modeString)
 	{
 		mode = Sine;
 	}
+	else if (modeString == "Quad")
+	{
+		mode = Quad;
+	}
+	else if (modeString == "Quart")
+	{
+		mode = Quart;
+	}
 	else if (modeString == "Cubic")
 	{
 		mode = Cubic;
 	}
+	else if (modeString == "Quint")
+	{
+		mode = Quint;
+	}
 	else if (modeString == "Expo")
 	{
 		mode = Expo;
+	}
+	else if (modeString == "Circ")
+	{
+		mode = Circ;
+	}
+	else if (modeString == "Back")
+	{
+		mode = Back;
 	}
 	else if (modeString == "Elastic")
 	{
